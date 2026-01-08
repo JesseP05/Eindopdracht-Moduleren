e6393206b7232c08bb7c6d651c9ccc3456bfa09e
JesseP05 <jesse.postma3108@gmail.com>
2026-01-08 17:34:39 +0100

    start graphing stuff also i hate matplotlib

--- grapher.py
+++ grapher.py
@@ -1,9 +1,50 @@
 """
-Module to help with graphing data with matplotlib (and openstreetmap?? )
+    Graphing module used by some of my scripts.
+    Author: Jesse Postma
 """
 
 
 import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+def graph_dates_bar(date_dict: dict[str, float], total_years : bool = False, tick_interval: int = 30, tick_rotation: int = 45, figsize: tuple[int, int] = (15,5)):
+    """UGH FUCK MATPLOTLIB
+
+    Args:
+        date_dict (dict[str, float]): _description_
+        total_years (bool, optional): _description_. Defaults to False.
+        tick_interval (int, optional): _description_. Defaults to 30.
+        tick_rotation (int, optional): _description_. Defaults to 45.
+        figsize (tuple[int, int], optional): _description_. Defaults to (15,5).
+    """
+    if total_years:
+        # average counts over years
+        total_dict = {}
+        for date_str, count in date_dict.items():
+            date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+            month_day_str = date_obj.strftime("%m-%d")
+            total_dict[month_day_str] = total_dict.setdefault(month_day_str, 0) + count
+        date_dict = total_dict
+        for k in date_dict:
+            date_dict[k] = date_dict[k] / 1000
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    ax.bar(list(date_dict.keys()), list(date_dict.values()),  color='orange', edgecolor = 'black')
+
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Number of incidents x1000')
+    ax.set_title('Total number of incidents per date since 2020')
+
+    ax.set_xticks(range(0, len(date_dict), tick_interval))
+    ax.tick_params(axis='x', rotation=tick_rotation)
+    ax.set_ylim(0, max(date_dict.values()) + 1)
+    ax.set_xmargin(0.01)
+    ax.grid(axis='y')
+
+    fig.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ Version: 0.2
 
 import numpy as np
 import pandas as pd
-import helper
+from datetime import datetime
+import helper, grapher
 
 
 def parse_mocodes(cell, map):
@@ -34,40 +35,63 @@ def process_data(files : list[str]):
     """Load and process the data files.
 
     Args:
-        files set(str): filepaths
+        files set(str): names of files in the data folder
     """
+    # prepend data/ to filenames
     for i, org_file in enumerate(files):
         file = f'data/{org_file}'
         files[i] = file
     
     lapd_df = pd.read_csv(files[0], engine='pyarrow')
+    lapd_df['DATE OCC'] = pd.to_datetime(lapd_df['DATE OCC'], format="%m/%d/%Y %I:%M:%S %p", errors='coerce') # fix formatting
+    lapd_df = lapd_df.sort_values(by='DATE OCC') # sort by date of incident
+    
     crim_cd_df = pd.read_csv(files[1], engine='pyarrow') # translation for criminal codes into classes
     rep_ds_df = pd.read_csv(files[2], engine='pyarrow') # gives info about bureau, type of unit
     stat_cd_df = pd.read_csv(files[3], engine='pyarrow') # translation for report status codes
     mcode_df = pd.read_csv(files[4], engine='pyarrow', dtype={"MO Code": int}) # information about what has reportedly happened in the event
 
-    # Map criminal codes to classified spectrums
-    #crim_cd_df['Criminal Code'] = pd.to_numeric(crim_cd_df['Criminal Code'])
+    # Map criminal codes to classes
     crim_cd_df = crim_cd_df.drop_duplicates(subset=['Criminal Code'])
 
+    # reparse reporting district to numeric and drop duplicates
     rep_ds_df['REPDIST'] = pd.to_numeric(rep_ds_df['REPDIST'])
     rep_ds_df = rep_ds_df.drop_duplicates(subset=['REPDIST'])
     
+    # new column for authority instead of overwriting rpt dist no
     lapd_df['Authority Type'] = lapd_df['Rpt Dist No'] # adding a column for type of unit (eg. sheriff or police)
-
+    
+    # replace codes with descriptive values
     lapd_df = helper.dictionary_replace_csv(lapd_df, crim_cd_df, 'Criminal Code', 'Crm Cd', 'Class')
     lapd_df = helper.dictionary_replace_csv(lapd_df, rep_ds_df, 'REPDIST', 'Rpt Dist No', 'BUREAU')
     lapd_df = helper.dictionary_replace_csv(lapd_df, rep_ds_df, 'REPDIST', 'Authority Type', 'S_TYPE')
-    lapd_df = helper.dictionary_replace_csv(lapd_df, stat_cd_df, 'status_code', 'Status', 'description') # adding more info to status description
+    lapd_df = helper.dictionary_replace_csv(lapd_df, stat_cd_df, 'status_code', 'Status', 'description')
     
+    
+    # mocode parsing is a bit harder because it can contain more than one code per cell
     # initialize a lookup map for mocodes
     mocode_map = dict(zip(mcode_df["MO Code"].astype(int), mcode_df["Description"]))
     # parse mocodes to readable desription
-    lapd_df["Mocodes Readable"] = lapd_df["Mocodes"].apply(parse_mocodes, args=(mocode_map,))
+    lapd_df["Mocodes Readable"] = lapd_df["Mocodes"].apply(parse_mocodes, args=(mocode_map,)) # trailing comma because of tuple req
     
-    print(mcode_df['MO Code'].head())
-    print(lapd_df['Mocodes'].head())
-    print(lapd_df['Mocodes Readable'].head())
+    return lapd_df
+
+
+def graph_dates(dates: list[str]):
+    """_summary_
+
+    Args:
+        dates (list[str]): _description_
+    """    
+    dates_dict = {}
+    for date in dates:
+        try:
+            dates_dict[date] = dates_dict.setdefault(date, 0) + 1
+        except Exception as e:
+            print(f"Invalid date format: {date}")
+            continue
+    grapher.graph_dates_bar(dates_dict, tick_interval=1, total_years=True, tick_rotation=90)
+
 
 def main():
     """
@@ -78,10 +102,12 @@ def main():
                     'LAPD_Reporting_District.csv', 'LAPD_Status_Codes.csv', 'mocodes.csv']
     helper.validate_project_structure(set(expected_files))
     
-    process_data(expected_files)
+    data = process_data(expected_files)
     
     # Function calls for specific graph we want
-    # graph_dates() # graph report count per day (switch between occ and rptd? prob just occ)
+    dates = data['DATE OCC'].astype(str).tolist()
+    graph_dates(dates) # graph report count per day (switch between occ and rptd? prob just occ)
+
     # graph_timep() # graph incident occ count per time on hourly interval
     # graph_dang_area() # bar chart for most dangerous areas -> count of rep/area (area name)
     # graph_victim_age() # bar chart
